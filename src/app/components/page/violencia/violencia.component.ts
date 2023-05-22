622fa2c128f95c75fc9973c55ca5d273033a4ced
Anuska <anuskazw@gmail.com>
2023-05-22 23:11:29 +0200

    fix: paginacion

---
+++ src/app/components/page/violencia/violencia.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-violencia',
+  templateUrl: './violencia.component.html',
+  styleUrls: ['./violencia.component.scss']
+})
+export class ViolenciaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
